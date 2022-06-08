c9028e8a6ddf8e3fb94058f772253c1498a10672
Sean Grant <seanetella@gmail.com>
2022-06-08 09:36:40 -0600

    String test

---
+++ variables.rb
@@ -0,0 +1,15 @@
+#strings
+email = "seanetella@gmail.com"
+name = "sean"
+sports = "climb"
+
+email = "jeff@gmail.com"
+name = "jeff"
+sports = "run"
+
+
+amount_due = "100.00"
+rent = "1500.00"
+# Booleans
+deposit_paid =
+first_class =
